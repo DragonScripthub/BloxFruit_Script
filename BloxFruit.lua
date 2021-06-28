c934cf34b51d4729a905d374c6936ec9832ea6a1
Azgeda-Hub <80238446+Azgeda-Hub@users.noreply.github.com>
2021-06-28 19:38:12 +0700

    Create BloxFruit.lua

---
+++ BloxFruit.lua
@@ -0,0 +1,60 @@
+local key = "1ciFGQyHM1dI"
+
+local http_request = http_request
+if syn then 
+    http_request = syn.request
+elseif SENTINEL_V2 then
+    http_request = request 
+end
+local getservice = game.GetService
+local httpservice = getservice(game, "HttpService")
+
+local function http_request_get(url, headers) 
+    return http_request({Url=url,Method="GET",Headers=headers or nil}).Body 
+end
+local function jsondecode(json)
+	local jsonTable = {}
+	 pcall(function() jsonTable = httpservice.JSONDecode(httpservice,json) end)
+	return jsonTable
+end
+
+local body = http_request_get('https://httpbin.org/get')
+local decoded = jsondecode(body)
+local hwid_list = {"Syn-Fingerprint", "Exploit-Guid", "Proto-User-Identifier", "Sentinel-Fingerprint"};
+local hwid = "";
+
+
+
+for i, v in next, hwid_list do
+    if decoded.headers[v] then
+        hwid = decoded.headers[v];
+        break
+    end
+end
+
+local data = jsondecode(http_request_get("http://localhost/server.php?key="..key))
+--print (data.Hwid)
+--print (hwid)
+if data.Key == key then
+    if data.Blacklist == "False" then
+        if data.Hwid == hwid then
+            print("Wait For Script")
+            --loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"))()
+        else
+            game.Players.LocalPlayer:Kick("คุณพยายามรันเครื่องอื่น")
+        end
+        if data.Hwid == "Unknown" then
+        -- update hwid
+        
+         http_request_get("http://localhost/changehwid.php?key=".. key .."&hwid="..hwid)
+         game.Players.LocalPlayer:Kick("\nUpdate Whitelist สำเร็จ\nรันใหม่อีกครั้ง")
+        else
+        -- no update hwid
+         --print("Whitelist !!!")
+         end
+    else
+        game.Players.LocalPlayer:Kick("\nKey ติด Blacklist")
+    end
+else
+    game.Players.LocalPlayer:Kick("\nKey ผิดกรุณาติดต่อแอดมิน\nหรือ Script ดับ")
+end
